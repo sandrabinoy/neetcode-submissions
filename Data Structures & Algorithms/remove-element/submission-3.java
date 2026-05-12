752deb1433847ba31e759e7f782ac7292efc82ce
Sandra Binoy <36753008+sandrabinoy@users.noreply.github.com>
2026-05-12 20:43:16 +0200

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if (nums.length < 1) return 0;
+
+        int p = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[p++] = nums[i];
+            }
+        }
+        return p;
+    }
+}
