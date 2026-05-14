ab119fd2be737fbf4a87d0af482cdde43f083994
Sandra Binoy <36753008+sandrabinoy@users.noreply.github.com>
2026-05-14 13:22:06 +0200

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+        if (nums == null || n == 0) return false;
+
+        boolean hasDupe = false;
+        Map<Integer, Integer> numsMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int ni = nums[i];
+            int j = numsMap.getOrDefault(ni, i);
+            if (j != i && i - j <= k) return true;
+            numsMap.put(ni, i);
+        }
+        return false;
+    }
+}
